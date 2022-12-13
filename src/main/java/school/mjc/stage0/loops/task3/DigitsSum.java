69074f5afed078ba2e39d04459a0ebf1608c4e7e
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-13 21:39:21 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+String plus = number + "";
+	    int sum = 0;
+	    int num = plus.length();
+	    for (int x = 1; x<=num; x++){
+	        sum+=number%10;
+	        number=number/10;
+	    }
+	    System.out.println(sum);
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+int lastFibonacci = 14;
+	    int viv1 = 0;
+	    int viv2 = 1;
+	    int sum;
+	    int num=1;
+	    for (int x = 1; x<=lastFibonacci; x++){
+	        System.out.println(num);
+	        sum=viv1+viv2;
+	        viv1 = viv2;
+	        viv2=sum;
+	        num++;
+	        if (viv2 >= lastFibonacci){
+	            break;}}
     }
 }
