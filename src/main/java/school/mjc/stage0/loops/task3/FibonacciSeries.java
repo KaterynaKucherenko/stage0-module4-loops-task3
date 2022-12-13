eb086a985ec60215bbb505521e3f20c52dec0b40
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-13 22:08:07 +0300

    fix4

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -5,15 +5,14 @@ public class FibonacciSeries {
 
 	    int viv1 = 0;
 	    int viv2 = 1;
-	    int sum;
+	    int sum=1;
 	    int num=1;
-	    for (int x = 1; x<=lastFibonacci; x++){
-	        System.out.println(num);
+	    System.out.println(viv1);
+	    for (int x = 2; x<=lastFibonacci; x++){
+	        System.out.println(sum);
 	        sum=viv1+viv2;
 	        viv1 = viv2;
-	        viv2=sum;
-	        num++;
-	        if (viv2 >= lastFibonacci){
-	            break;}}
+	        viv2 = sum;
+	    }
     }
 }
